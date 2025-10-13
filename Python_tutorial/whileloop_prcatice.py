597afeb1294e4083d7af8af96e33d904766add75
saksham-v25 <vishwakarmasaksham07@gmail.com>
2025-10-13 21:12:46 +0530

    Add files via upload

---
+++ Python_tutorial/whileloop_prcatice.py
@@ -0,0 +1,8 @@
+number = int(input("enter a number(-1 to Quit):"))
+while number!=-1:
+    print(number)
+    number = int(input("enter a number(-1 to Quit):"))
+else:
+    print("in else block ")
+
+print("out from loop")
