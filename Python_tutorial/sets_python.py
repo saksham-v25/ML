2a49794a93d9da76792d620ae2ca4ac13653454f
saksham-v25 <vishwakarmasaksham07@gmail.com>
2025-10-16 18:59:17 +0530

    Add files via upload

---
+++ Python_tutorial/sets_python.py
@@ -0,0 +1,6 @@
+#duplicate are not allowed in set
+#suport multiple datatypes in a single set
+#you cannot access data from sets usong index number
+#sets are immutable
+#set store data im unodered
+#indexing and slicing are not allowed
